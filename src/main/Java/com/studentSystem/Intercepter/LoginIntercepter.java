0a72fed0c1eebd239fe3f1a291768754f427af08
Anonymous <447192078@qq.com>
2019-03-31 12:58:57 +0800

    '解决bug中'

--- src/main/Java/com/studentSystem/Intercepter/LoginIntercepter.java
+++ src/main/Java/com/studentSystem/Intercepter/LoginIntercepter.java
@@ -1,12 +1,13 @@
 package com.studentSystem.Intercepter;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+@Component
 public class LoginIntercepter implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
