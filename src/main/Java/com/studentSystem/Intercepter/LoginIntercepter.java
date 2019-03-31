fd4ca29d94ed28d3749f876f260647ef9464ee23
Anonymous <447192078@qq.com>
2019-03-31 10:21:34 +0800

    修改拦截器包名问题

--- src/main/Java/com/studentSystem/Intercepter/LoginIntercepter.java
+++ src/main/Java/com/studentSystem/Intercepter/LoginIntercepter.java
@@ -1,4 +1,4 @@
-package com.studentSystem.LoginIntercepter;
+package com.studentSystem.Intercepter;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
